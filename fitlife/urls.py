98f3dd4afda0d7a8299ea03a3215d466b3be1571
MLMorais <marialuiza2002moraes@gmail.com>
2026-06-22 01:04:44 -0300

    update: urls.py
    Atualização das urls envolvendo a entidade aluno

--- fitlife/urls.py
+++ fitlife/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('alunos/', views.listar_alunos, name='listar_alunos'),
+    path('alunos/cadastro/', views.cadastrar_aluno, name='cadastrar_aluno'),
+    path('alunos/<str:cpf>/editar/', views.editar_aluno, name='editar_aluno'),
+    path('alunos/<str:cpf>/', views.detalhe_aluno, name='detalhe_aluno'),
+    path('alunos/<str:cpf>/excluir/', views.excluir_aluno, name='excluir_aluno'),
 ]
